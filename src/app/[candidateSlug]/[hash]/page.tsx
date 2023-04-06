31fa79082dc5dcfafd81eef0efb62edc33a61481
Christian Cito <citochris@gmail.com>
2023-04-06 17:39:49 +0200

    fix error caused by falsy 0 value

--- src/app/[candidateSlug]/[hash]/page.tsx
+++ src/app/[candidateSlug]/[hash]/page.tsx
@@ -63,7 +63,7 @@ export default async function CandidatePreWahlkabine({
               <h2 className="text-2xl mb-5 hyphens-auto">
                 {answer.question.title}
               </h2>
-              {answer.option && answer.weighting ? (
+              {answer.option !== null && answer.weighting !== null ? (
                 <div className="grid grid-cols-2 gap-5">
                   <p className="border-brand bg-red-50/50 text-center px-3 py-2 text-gray-800 underline underline-offset-2">
                     {optionLabelForValue(answer.option)
