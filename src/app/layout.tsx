94a9b17bce2390caf257ae987b80026f63f366da
Georg Windhaber <georg@journiapp.com>
2023-04-15 20:20:25 +0200

    Change main header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Script from "next/script";
+import Image from "next/image";
 
 export const metadata = {
   title: "SPÖ Vorsitz Wahlkabine | Junge Generation Wien",
@@ -36,17 +37,35 @@ export default function RootLayout({
         ></Script>
       </head>
       <body className={interFont.variable}>
-        <div className={`w-full min-h-full z-10 flex flex-col px-5`}>
+        <div className={`w-full min-h-full z-10 flex flex-col`}>
           <div className="lg:w-[800px] w-full flex flex-col min-h-full flex-grow mx-auto gap-5 md:gap-10">
-            <header className="mt-10">
+            <header className="flex flex-col md:flex-row items-center md:items-end  bg-primary-100 font-brand p-3">
               <Link
                 href="/"
-                className="text-2xl inline-flex text-white bg-brand p-3 focus-visible:outline-brand outline-offset-2"
+                className="text-3xl text-center sm:text-right leading-none font-bold inline-flex focus-visible:outline-brand outline-offset-2"
               >
                 SPÖ Vorsitzwahl-Kabine
               </Link>
+              <div className="flex-1"></div>
+              <div className="flex items-end mt-1 md:mt-0">
+                <span className="mr-2 md:mx-2">powered by</span>
+                <a
+                  target="_blank"
+                  rel="norefeerer noopener"
+                  className="focus-visible:outline-brand outline-offset-2"
+                  href="https://junge-generation.at/"
+                >
+                  <Image
+                    src="/icon-block-white.png"
+                    height={36}
+                    width={181.83}
+                    className="object-contain flex-1"
+                    alt="JG Logo"
+                  />
+                </a>
+              </div>
             </header>
-            <main className="flex-grow">{children}</main>
+            <main className="flex-grow px-5">{children}</main>
             <footer className="w-full flex justify-center items-center py-5 border-t border-brand">
               <ul className="flex flex-row gap-3 justify-center flex-wrap">
                 <li>
@@ -64,7 +83,7 @@ export default function RootLayout({
                     target="_blank"
                     rel="norefeerer noopener"
                     className="focus-visible:outline-brand outline-offset-2"
-                    href="https://www.spoe.at/junge-generation"
+                    href="https://junge-generation.at/"
                   >
                     Junge Generation
                   </a>
