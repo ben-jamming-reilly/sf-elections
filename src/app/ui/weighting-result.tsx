23d5739c5f2eb3c3652da3a445e398a138ac7263
Georg Windhaber <georg@journiapp.com>
2023-04-15 23:47:10 +0200

    Add bg to candidate answer

--- src/app/ui/weighting-result.tsx
+++ src/app/ui/weighting-result.tsx
@@ -14,7 +14,7 @@ export const WeightingResult = ({ value }: { value: number }) => {
   return (
     <p
       className={clsx(
-        "text-center px-3 py-2 bg-surface-200 selection:text-white inline-flex items-center justify-center text-lg font-medium gap-2"
+        "text-center px-3 py-2 selection:text-white inline-flex items-center justify-center text-lg font-medium gap-2"
       )}
     >
       {value === 0 && <ChevronDoubleDownIcon className={IconClasses} />}
