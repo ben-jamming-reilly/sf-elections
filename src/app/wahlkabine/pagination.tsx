8005a84afd8c05cccdc9ccf789cfdbfd1dd6edaf
Georg Windhaber <georg@journiapp.com>
2023-04-15 19:03:51 +0200

    Color-code pagination

--- src/app/wahlkabine/pagination.tsx
+++ src/app/wahlkabine/pagination.tsx
@@ -30,20 +30,17 @@ const Pagination = ({
         <li key={`question-shortcut-${question.id}`}>
           <button
             className={clsx(
-              "inline-flex justify-center items-center w-[2em] h-[2em] transition-all underline-offset-2 hover:border-brand hover:underline border",
+              "inline-flex justify-center items-center w-[2em] h-[2em] transition-all underline-offset-2 hover:border-brand hover:underline border border-surface-400",
               isQuestionAnswered(question) &&
                 !question.skipped &&
-                "bg-brand text-white border-brand",
+                "bg-primary-100 hover:bg-primary-200 border-primary-100",
               isQuestionAnswered(question) &&
                 question.skipped &&
-                "bg-neutral-200 text-neutral-500 border-neutral-200 hover:border-neutral-200",
-              activeQuestion.id === question.id && "underline scale-[0.85]",
+                "bg-surface-400 border-surface-600 hover:border-primary-100",
+              activeQuestion.id === question.id && "underline scale-[0.875]",
               !isQuestionAnswered(question) &&
                 activeQuestion.id === question.id &&
-                "border-brand bg-transparent hover:text-brand text-brand",
-              activeQuestion.id !== question.id &&
-                !isQuestionAnswered(question) &&
-                "bg-red-50/50 hover:text-brand"
+                "border-primary-100"
             )}
             onClick={() => setActiveIndex(index)}
           >
