7a58178b51167bd04fb97e5ca630c93c2cb8b3b9
Georg Windhaber <georg@journiapp.com>
2023-04-16 15:46:25 +0200

    Nightmoded single candidate comparison

--- src/app/wahlkabine/[slug]/[candidate-slug]/page.tsx
+++ src/app/wahlkabine/[slug]/[candidate-slug]/page.tsx
@@ -60,10 +60,10 @@ export default async function Wahlkabine({
 
   return (
     <div>
-      <div className="flex flex-row items-center gap-2 mb-10">
+      <div className="flex flex-row items-center justify-between gap-2 mb-10">
         <Link
           href={`/wahlkabine/${params.slug}`}
-          className="border-brand border  px-3 py-2 hover:bg-brand hover:text-white active:scale-95 inline-flex items-center justify-center transition-all rounded-md text-brand gap-2"
+          className="border-brand border  px-3 py-2 hover:bg-primary-100 hover:text-white inline-flex items-center justify-center transition-all text-primary-100 rounded-md gap-2"
         >
           <ArrowLeftCircleIcon className="w-5 h-5 stroke-2" />
           Zurück zur Übersicht
@@ -75,7 +75,7 @@ export default async function Wahlkabine({
 
       <section className="grid grid-cols-1 md:grid-cols-3">
         <div key={candidate.id} className="py-5 rounded-md relative">
-          <span className="absolute z-30 rounded-full w-14 h-14 top-2 -left-3 bg-brand text-white tabular-nums inline-flex justify-center items-center">
+          <span className="absolute z-30 rounded-full w-14 h-14 top-2 -left-3 bg-primary-100 tabular-nums inline-flex justify-center items-center">
             {percentage}%
           </span>
           <div className="">
@@ -91,41 +91,37 @@ export default async function Wahlkabine({
                 className=" group-hover:scale-110 ease-in-out transition-all  bg-brand-yellow w-full"
               />
             </Link>
-            <h2 className="text-2xl bg-brand text-white font-medium hyphens-auto px-3 py-2 selection:text-brand selection:bg-white text-center w-[110%] -translate-x-[5%] shadow-md">
+            <h2 className="text-2xl bg-primary-100 font-medium hyphens-auto px-3 py-2 selection:text-brand selection:bg-white text-center w-[110%] -translate-x-[5%] shadow-md">
               {candidate.name}
             </h2>
-            <div className="p-5 border-2 z-20 relative rounded-br-md rounded-bl-md border-t-0 bg-white border-gray-800">
+            <div className="p-5 z-20 bg-surface-200 relative rounded-b-md">
               <p className="prose mb-5">{candidate.description}</p>
 
               <Link
-                className="text-white selection:text-brand-purple selection:bg-white inline-block active:scale-95 transition-all bg-brand-purple px-4 py-2 rounded-md hover:underline"
+                className="text-white selection:text-brand-purple selection:bg-white inline-block active:scale-95 transition-all bg-primary-100 px-4 py-2 rounded-md hover:underline"
                 href={`/${candidate.slug}`}
               >
-                Profile
+                {`${candidate.name}s Antworten`}
               </Link>
             </div>
           </div>
         </div>
       </section>
 
-      <ul className="flex flex-col divide-y-2">
+      <ul className="flex flex-col">
         {voterWithAnswers.answers.map((answer, index) => (
           <li key={answer.id} className="py-5">
             {answer.question.category && (
               <QuestionCategoryLabel category={answer.question.category} />
             )}
-            <div className="text-lg font-semibold">
-              Frage {answer.questionId}:
-            </div>
-            <h2 className="text-xl md:text-2xl mb-2 md:mb-5 hyphens-auto">
+            <div className="text-lg">Frage {answer.questionId}:</div>
+            <h2 className="text-xl md:text-2xl mb-2 md:mb-5 hyphens-auto font-brand">
               {answer.question.title}
             </h2>
 
             <div className="grid grid-cols-1 py-5 sm:grid-cols-2 gap-5">
-              <div className="flex flex-col gap-1">
-                <div className="text-center top-0 bg-white py-2">
-                  Deine Antwort:
-                </div>
+              <div className="flex flex-col gap-1 bg-surface-200 p-3 rounded-md">
+                <div className="text-center top-0 py-2">Deine Antwort:</div>
                 {answer.option !== null && answer.weighting !== null ? (
                   <div className="grid grid-cols-1 grid-rows-2 gap-5">
                     <OptionResult
@@ -141,8 +137,8 @@ export default async function Wahlkabine({
                 )}
               </div>
 
-              <div className="flex flex-col gap-1">
-                <div className="justify-center inline-flex flex-row items-center gap-2 mb-2 top-0 bg-white">
+              <div className="flex flex-col gap-1 bg-surface-200 p-3 rounded-md">
+                <div className="justify-center inline-flex flex-row items-center gap-2 mb-2 top-0">
                   Anwort von{" "}
                   <span className="inline-flex items-center gap-1">
                     <Image
